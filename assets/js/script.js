64595249d857b806298d77badd7cc1b86405c9f5
Sarah Richens <sarahgrichens@gmail.com>
2023-01-11 19:15:14 -0600

    test out save button event listener and extract text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,18 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  let saveButtonEl = $('#save');
+  let textEl = $('#text'); 
+  //let userEvent = textEl.value;
+
+  saveButtonEl.on('click', function () {
+    console.log('hi'); 
+    //textEl.text('hi');
+    let userEvent = textEl.val(); console.log(userEvent);
+    
+  })
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -19,7 +31,7 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  
+
   // TODO: Add code to display the current date in the header of the page.
 
   let today = dayjs();
